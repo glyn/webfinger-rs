d3d9580e15345a096c3e94c45638ec8409401443
Glyn Normington <glyn.normington@gmail.com>
2024-05-30 15:47:49 +0100

    Stuck attempting to return an async function that captures a string.
    https://stackoverflow.com/questions/61167939/return-an-async-function-from-a-function-in-rust

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,10 @@ If not, see <https://www.gnu.org/licenses/>.
 */
 
 use std::convert::Infallible;
+use std::fs;
 use std::net::SocketAddr;
+
+use clap::Parser;
 
 use http_body_util::Full;
 use hyper::body::Bytes;
@@ -26,35 +29,60 @@ use hyper::{Request, Response};
 use hyper_util::rt::TokioIo;
 use tokio::net::TcpListener;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// File path of webfinger JSON file
+    #[arg(short, long)]
+    file_path: String,
+}
+
+type Hellofn = fn(Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible>;
+
+fn make_hello(jrd : String) -> Hellofn {
+    || {
+        Ok(Response::new(Full::new(Bytes::from(jrd))))
+    }
+}
+
 async fn hello(_: Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
     Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+fn main() {
+    let args = Args::parse();
 
-    // We create a TcpListener and bind it to 127.0.0.1:3000
-    let listener = TcpListener::bind(addr).await?;
+    let webfinger_jrd = fs::read_to_string(args.file_path)
+        .expect("Failed to read file");
 
-    // We start a loop to continuously accept incoming connections
-    loop {
-        let (stream, _) = listener.accept().await?;
+    let rt = tokio::runtime::Runtime::new().unwrap();
 
-        // Use an adapter to access something implementing `tokio::io` traits as if they implement
-        // `hyper::rt` IO traits.
-        let io = TokioIo::new(stream);
-
-        // Spawn a tokio task to serve multiple connections concurrently
-        tokio::task::spawn(async move {
-            // Finally, we bind the incoming connection to our `hello` service
-            if let Err(err) = http1::Builder::new()
+    let result : Result<(), Box<dyn std::error::Error + Send + Sync>>  = rt.block_on(async {
+        let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+        
+        // We create a TcpListener and bind it to 127.0.0.1:3000
+        let listener = TcpListener::bind(addr).await?;
+        
+        // We start a loop to continuously accept incoming connections
+        loop {
+            let (stream, _) = listener.accept().await?;
+            
+            // Use an adapter to access something implementing `tokio::io` traits as if they implement
+            // `hyper::rt` IO traits.
+            let io = TokioIo::new(stream);
+            
+            // Spawn a tokio task to serve multiple connections concurrently
+            tokio::task::spawn(async move {
+                // Finally, we bind the incoming connection to our `hello` service
+                if let Err(err) = http1::Builder::new()
                 // `service_fn` converts our function in a `Service`
-                .serve_connection(io, service_fn(hello))
+                .serve_connection(io, service_fn(make_hello(webfinger_jrd)))
                 .await
-            {
-                eprintln!("Error serving connection: {:?}", err);
-            }
-        });
-    }
+                {
+                    eprintln!("Error serving connection: {:?}", err);
+                }
+            });
+        }
+    });
+    result.unwrap();
 }
